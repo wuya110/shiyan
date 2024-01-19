f1f17882018b27e341cfaeab56ce48e68051e3a4
wuya110 <147088502+wuya110@users.noreply.github.com>
2024-01-19 23:56:20 +0800

    Create 6.js

---
+++ 6.js
@@ -0,0 +1,17 @@
+const cookieName = '全民K歌'
+const cookieKey = 'cookie_qmkg'
+const url = $request.url;
+
+if (url.indexOf("https://node.kg.qq.com/log?format=json&outCharset=utf-8&g_tk") >= 0) {
+  const cookieValue = $request.headers['Cookie'];
+  if (cookieValue) {
+    $prefs.setValueForKey(cookieValue, cookieKey);
+    $notify(cookieName, '获取Cookie成功', '');
+  } else {
+    $notify(cookieName, '获取Cookie失败', '无法从请求中提取Cookie');
+  }
+} else {
+    $notify(cookieName, '获取Cookie失败', 'URL不匹配');
+}
+
+$done({});
